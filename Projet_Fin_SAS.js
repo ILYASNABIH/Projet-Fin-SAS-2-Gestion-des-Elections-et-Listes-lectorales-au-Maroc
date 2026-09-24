3c4a539cbe41e049ce924ad170be3a45fac82bb0
ILYASNABIH <IlyasNabih17@hotmail.com>
2026-09-24 20:36:52 +0000

    Ajout de choix et affichage

--- Projet_Fin_SAS.js
+++ Projet_Fin_SAS.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync') ();
+let p;
+let c;
 const candidats = [{
 cin : "AB123456",
 nom : "Boushaba",
@@ -13,3 +16,25 @@ partiPolitique: "Independant",
 age: 65,
 electeurs: []
 }];
+do {
+    console.log("-----------------MENU---------------");
+    console.log("Tapez 1 pour ajouter un nouveau candidat ");
+    console.log("Tapez 2 pour afficher ");
+    console.log("Tapez 3 pour voter");
+    console.log("Tapez 4 pour modifier");
+    console.log("Tapez 5 pour éliminer un candidat");
+    console.log("Tapez 6 pour rechercher sur un candidat");
+    console.log("Tapez 7 pour afficher les statistiques")
+    console.log("Tapez 0 pour quitter le programme")
+    p = Number(prompt("choisir : "))
+    switch(p) {
+        case 1:
+            break;
+        case 2:
+            console.clear()
+            console.table(candidats)
+            c = prompt("continue...")
+            break;
+
+    }
+} while (p!==0)
