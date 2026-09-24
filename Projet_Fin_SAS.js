14445d07092f0ee7a228f0dfb203a1da1981bb04
ILYASNABIH <IlyasNabih17@hotmail.com>
2026-09-24 19:24:03 +0000

    Initialisation de Projet

---
+++ Projet_Fin_SAS.js
@@ -0,0 +1,15 @@
+const candidats = [{
+cin : "AB123456",
+nom : "Boushaba",
+prenom : "Soufiane",
+partiPolitique : "Indépendant",
+age: 40,
+electeurs: []
+}, {
+cin: "CD987654",
+nom: "Akhenoch",
+prenom: "Aziz",
+partiPolitique: "Independant",
+age: 65,
+electeurs: []
+}];
