5bf3082ce7b78b1c01f6553f6deedffff9fc0099
ILYASNABIH <IlyasNabih17@hotmail.com>
2026-09-25 09:40:29 +0000

    Ajout d'un nouveau candidat

--- Projet_Fin_SAS.js
+++ Projet_Fin_SAS.js
@@ -2,22 +2,22 @@ const prompt = require('prompt-sync') ();
 let p;
 let c;
 const candidats = [{
-cin : "AB123456",
-nom : "Boushaba",
-prenom : "Soufiane",
-partiPolitique : "Indépendant",
-age: 40,
-electeurs: []
+CIN : "AB123456",
+Nom : "Boushaba",
+Prénom : "Soufiane",
+PartiPolitique : "Indépendant",
+Age: 40,
+Electeurs: ["KL123456", "YI918273", "G784593", "M974310"]
 }, {
-cin: "CD987654",
-nom: "Akhenoch",
-prenom: "Aziz",
-partiPolitique: "Independant",
-age: 65,
-electeurs: []
+CIN: "CD987654",
+Nom: "Akhenoch",
+Prénom: "Aziz",
+PartiPolitique: "Independant",
+Age: 65,
+Electeurs: ["N987654", "J123456" , "H123678"]
 }];
 do {
-    console.log("-----------------MENU---------------");
+    console.log("-------------------MENU------------------");
     console.log("Tapez 1 pour ajouter un nouveau candidat ");
     console.log("Tapez 2 pour afficher ");
     console.log("Tapez 3 pour voter");
@@ -29,6 +29,15 @@ do {
     p = Number(prompt("choisir : "))
     switch(p) {
         case 1:
+            const cin = prompt("Tapez le CIN de candidat : ");
+            const nom = prompt("Tapez le nom de candidat : ");
+            const prenom = prompt("Tapez le prénom de candidat : ");
+            const partipolitique = prompt("Tapez la parti politique de candidat : ");
+            const age = Number(prompt("Tapez l'age de candidat : "));
+            const electeurs = prompt("Tapez les electeurs de candidat : ");
+            candidats.push({CIN : cin, Nom : nom, Prénom: prenom, PartiPolitique: partipolitique, Age: age, Electeurs : electeurs.split(" ") })
+            console.log("le candidat a ete ajoute avec succes")
+            c = prompt("continue...")
             break;
         case 2:
             console.clear()
